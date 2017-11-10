b90ecac2d7036d4477ba64241a03fc847fdc5992
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2017-11-10 14:24:28 -0500

    Release 2.1.1.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ fstd_core = Extension ('pygeode.formats.fstd_core', sources=['fstd_core.c'], lib
 # PyGeode installation script
 
 setup (	name="pygeode-rpn",
-	version="2.1.0",
+	version="2.1.1",
         author="Mike Neish",
         install_requires=['pygeode'],
 	ext_modules=[fstd_core],
