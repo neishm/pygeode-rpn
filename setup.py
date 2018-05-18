de724f577d579991a7eb5ca672df7e522a7ea7d1
Mike Neish <Michael.Neish@canada.ca>
2018-05-18 16:31:21 -0400

    Instead of installing directly into pygeode.formats namespace, install in a separate location and use entry_points to hook into PyGeode.
    Requires PyGeode >= 1.2.0 for this to work.

--- setup.py
+++ setup.py
@@ -4,18 +4,24 @@ from setuptools import setup, Extension
 import numpy as np
 
 # Extension modules
-fstd_core = Extension ('pygeode.formats.fstd_core', sources=['fstd_core.c'], libraries=['rmn'])
+fstd_core = Extension ('pygeode_rpn.fstd_core', sources=['fstd_core.c'], libraries=['rmn'])
 
 # PyGeode installation script
 
 setup (	name="pygeode-rpn",
 	version="2.1.1",
         author="Mike Neish",
-        install_requires=['pygeode'],
+        install_requires=['pygeode >= 1.2.0'],
 	ext_modules=[fstd_core],
         include_dirs = [np.get_include()],
-	packages=["pygeode.formats"],
-        package_dir = {"pygeode.formats":""},
+	packages=["pygeode_rpn"],
+        package_dir = {"pygeode_rpn":""},
+        entry_points = {
+          'pygeode.formats': [
+            'fstd = pygeode_rpn.fstd',
+            'fstd_core = pygeode_rpn.fstd_core',
+          ],
+        },
 	scripts=["fstd2nc"]
 )
 
